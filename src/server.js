8afd12c9f51b2be0ac3d9d52dfea965d4917f0ac
Jett Jagger <jettrjagger@hotmail.com>
2023-09-21 10:45:37 +0100

    add db connection

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ const app = express ();
 
 app.use(express.json());
 
+//http://localhost/health
+app.get("/health", (req,res) => {
+    res.status(200).json({message: "API is healthy"});
+});
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
