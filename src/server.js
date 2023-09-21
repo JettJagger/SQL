7af443ca13f220589a10f4bc2eec0146be138c90
Jett Jagger <jettrjagger@hotmail.com>
2023-09-21 11:09:29 +0100

    add Boo model server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,24 @@ const app = express ();
 
 app.use(express.json());
 
+const {DataTypes} = require("sequalize");
+const connection = require("./db/connection");
+
+const Book = connection.define("Book", {
+    title:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    author: {
+        type: DataTypes.STRING,
+    },
+    genre: {
+        type: DataTypes.STRING,
+    },
+
+});
+
 //http://localhost/health
 app.get("/health", (req,res) => {
     res.status(200).json({message: "API is healthy"});
